7865968df070938be71146c66c89cc65efa8d310
Alex <alex.hodge@gmail.com>
2016-10-05 00:28:44 +1300

    Create expo executable
    This commit integrates the modules from previous into a minimum
    viable product. There is limited error handling and input
    checking but there is some.
    
    Further testing required but this looks to be close to the spec.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,55 @@
 #include <iostream>
+#include "exponentiator.h"
+#include "reverse.h"
+#include <exception>
+#include <stdlib.h>
+
+using namespace std;
+
+const string USAGE_INFO("Usage: <integer[0-99999]>");
+
+static bool input_is_valid(string input)
+{
+    bool retval = true;
+    if (input.length() > 5 ||
+    !all_of(input.begin(), input.end(), ::isdigit))
+    {
+        retval = false;
+    }
+
+    return retval;
+}
+
+static string get_input()
+{
+    string input = "";
+    while (true) {
+        cout << "Enter an integer number (0-99999): ";
+        getline(cin, input);
+        if (input_is_valid(input))
+            break;
+        cout << "Invalid, please try again" << endl;
+    }
+
+    return input;
+}
 
 int main()
 {
-    printf("Hello!\n");
+    using exponentiator::Exponentiator;
+    string input_string(get_input());
+
+    try
+    {
+        unsigned long int input = stol(input_string);
+        Exponentiator expo(reverse);
+        cout << expo.run(input) << endl;
+    }
+    catch(exception& e)
+    {
+        cout << e.what() << endl;
+        return -1;
+    }
+
     return 0;
 }
